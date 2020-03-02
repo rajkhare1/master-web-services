4b09366d6fc01da5d7ca6835583a8107a2f10008
rajkhare <rajkhare@192.168.31.81>
2020-03-02 15:40:28 +0530

    update for SOAP xsd

---
+++ soap-course-management/src/test/java/com/raj/soap/webservices/SoapCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.soap.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
